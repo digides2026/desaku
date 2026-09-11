5b6efc724a208825960d1455013e590d9d6ea769
ahmadfd-afk <id.localcredit@gmail.com>
2026-09-12 00:15:43 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -38,12 +38,12 @@ window.__DB_CONFIG__ = {
 
   // Firebase config (dipakai bila provider = 'firebase*')
   firebase: {
-    apiKey:            "",
-    authDomain:        "",
-    databaseURL:       "",  // wajib untuk Realtime Database
-    projectId:         "",
-    storageBucket:     "",
-    messagingSenderId: "",
-    appId:              ""
+    apiKey:            "AIzaSyBqBOSu1JCRRdODIra5ss1LIVzcs80i5tQ",
+    authDomain:        "punikan-e1cac.firebaseapp.com",
+    databaseURL:       "https://punikan-e1cac-default-rtdb.asia-southeast1.firebasedatabase.app",  // wajib untuk Realtime Database
+    projectId:         "punikan-e1cac",
+    storageBucket:     "punikan-e1cac.firebasestorage.app",
+    messagingSenderId: "40232508378",
+    appId:              "1:40232508378:web:141e482fb7e82c9d193ee3"
   }
 };
